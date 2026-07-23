e7179a15a621725d5cc4ffb42406470ced640344
Rachit Agarwal <rachit4907@gmail.com>
2026-07-23 09:19:31 +0530

    Time: 0 ms (100%), Space: 14 MB (85.12%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+     void func(int ind, int sum, vector<int> &nums, 
+              vector<int> &candidates, vector<vector<int>> &ans){
+                  if(sum == 0) {
+            ans.push_back(nums);
+            return;
+        }
+        if(sum < 0 || ind == candidates.size()) return; 
+         nums.push_back(candidates[ind]);
+         func(ind + 1, sum - candidates[ind], nums, candidates, ans); 
+          nums.pop_back();  
+              
+               for(int i = ind + 1; i < candidates.size(); i++) {
+            if(candidates[i] != candidates[ind]) {
+                func(i, sum, nums, candidates, ans); 
+                break; 
+            }}
+        }
+    vector<vector<int> > combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> ans; 
+        vector<int> nums; 
+           sort(candidates.begin(), candidates.end()); 
+              func(0, target, nums, candidates, ans);
+               return ans; 
+    }
+};
