df80458e4beaee834a826451572e471f4e1f7ff5
Rachit Agarwal <rachit4907@gmail.com>
2026-08-30 16:58:38 +0530

    Time: 0 ms (100%), Space: 14 MB (57.23%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,18 +1,22 @@
 class Solution{	
 	public:		
 		vector<int> singleNumber(vector<int>& nums){
-            int n =nums.size();
-            unordered_map <int , int > mpp;
-              vector<int> ans;
-            for(int i = 0;i<n;i++){
-                mpp[nums[i]]++;
-                }
-             for(auto it : mpp){
-                if(it.second==1){
-                    ans.push_back(it.first);
-                }
+		 int n = nums.size();
+         long XOR =0;
+         for(int i=0;i<n;i++){
+            XOR ^=nums[i];
+         }
+         int rm = (XOR &(XOR -1))^ XOR;
+         int XOR1=0,XOR2 =0;
+            for(int i=0; i < n; i++) {
+                  if(nums[i] & rm) {
+                XOR1 = XOR1 ^ nums[i];
             }
-               sort(ans.begin(), ans.end());
-        return ans;
+            else {
+                XOR2 = XOR2 ^ nums[i];
+            }
+        }
+          if(XOR1 < XOR2) return {XOR1, XOR2};
+        return {XOR2, XOR1};
 		}
 };
